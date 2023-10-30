8975afbc0ee3804cfaec26fa4c19b576bd49a9dd
Mereke Sabitov <merekesabitov@gmail.com>
2023-10-30 22:37:58 +0500

    no message

--- Learning_Dart/bin/Lists/ex2_match_in_the_list.dart
+++ Learning_Dart/bin/Lists/ex2_match_in_the_list.dart
@@ -7,7 +7,7 @@
 Список = [60, 999, 14, "dart1", 45, 95, "dart", 1]
 
 Список может содержать числа или строки. Value должно возвращать true,
-если список содержит значение, и false, если нет. Запрещено использовать indexOf.
+если список содержит значение, и false, если нет. Запрещено использовать indexOf и contains.
 
 Входные данные:
 Значение = “dart”
@@ -25,21 +25,29 @@ final collection = [60, 999, 14, "dart1", 45, 95, "dart", 1];
 final element1 = "dart";
 final element2 = 15;
 
+//specifying the type directly
 match(List<Object> list, Object element) {
-  var matches = false;
-
   for(var i in list) {
     if(i == element) {
-      matches = true;
-      break;
+      return true;
     }
   }
-  return matches;
+  return false;
+}
+
+//with Generics
+bool match2<T>(List<T> list, T element) {
+  for(var i in list) {
+    if(i == element) {
+      return true;
+    }
+  }
+  return false;
 }
 
 void main() {
   final test1 = match(collection, element1);
   print(test1);
-  final test2 = match(collection, element2);
+  final test2 = match2(collection, element2);
   print(test2);
 }
