e8c5e1bb7b4cd6f2ce5f6301a6a174258fee064f
Mereke Sabitov <merekesabitov@gmail.com>
2024-02-07 00:42:50 +0500

    Network_Requests (POST request)

--- Learning_Dart/bin/08(2)_NetworkRequests/api_service.dart
+++ Learning_Dart/bin/08(2)_NetworkRequests/api_service.dart
@@ -4,15 +4,15 @@ import 'package:http/http.dart' as http;
 import 'post.dart';
 
 class APIService {
-  Future<void> fetchPosts() async {
-    final url = Uri.parse('http://localhost:3000/posts');
+  final url = Uri.parse('http://localhost:3000/posts');
 
+  Future<void> fetchPosts() async {
     try {
       final response = await http.get(url);
 
       if (response.statusCode != 200) {
         throw HttpException(
-            'Не удалось получить данные. Код сервер: ${response.statusCode}');
+            'Не удалось получить данные. Код сервера: ${response.statusCode}');
       }
 
       final jsonData = response.body;
@@ -23,6 +23,30 @@ class APIService {
       for (final i in posts) {
         print(
             '\nPOST ID: ${i.id}, \nTitle: ${i.title}, \nDescription: ${i.body}');
+      }
+    } on SocketException {
+      print('Не удалось получить доступ к ресурсу');
+    } on FormatException {
+      print('Неправильный формат данных');
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> createPost(Post newPost) async {
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(newPost.toJson()),
+      );
+
+      if (response.statusCode == 201) {
+        final createdPost = Post.fromJson(json.decode(response.body));
+        print('Пост успешно создан!\nPost ID: ${createdPost.id}');
+      } else {
+        throw HttpException(
+            'Не удалось создать пост. Код сервера: ${response.statusCode}');
       }
     } on SocketException {
       print('Не удалось получить доступ к ресурсу');
--- Learning_Dart/bin/08(2)_NetworkRequests/main.dart
+++ Learning_Dart/bin/08(2)_NetworkRequests/main.dart
@@ -1,6 +1,14 @@
 import 'api_service.dart';
+import 'post.dart';
 
 void main() async {
   print('Получение постов...');
   await APIService().fetchPosts();
+
+  print('\n---------------------------\nСоздание нового поста...');
+  final newPost = Post(id: 3, title: 'Post 3', body: 'Body of Post 3');
+  await APIService().createPost(newPost);
+
+  print('\n---------------------------\nОбновленный список постов...');
+  await APIService().fetchPosts();  
 }
