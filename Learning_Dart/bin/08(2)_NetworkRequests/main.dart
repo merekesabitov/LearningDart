88746ec3f9bf229675a2d4fabb13570e9c2edb94
Mereke Sabitov <merekesabitov@gmail.com>
2024-02-10 18:14:52 +0500

    Network_Request (DELETE request)

--- Learning_Dart/bin/08(2)_NetworkRequests/main.dart
+++ Learning_Dart/bin/08(2)_NetworkRequests/main.dart
@@ -3,23 +3,26 @@ import 'post.dart';
 
 final existKeys = <String>{};
 
-void main() async {
+//Commands with server operation
+//--Start server: npx json-server db.json
+//-- Stop server: ctrl + C
 
+void main() async {
   //GET request
   print('Получение постов...');
   await APIService().fetchPosts();
-  
+
   //POST request
   final newPost = Post(id: "3", title: 'Post 3', body: 'Body of Post 3');
   await APIService().createPost(newPost);
 
   //PUT request
   final updateID = '3';
-  final updatePost = Post(id: updateID, title: 'Updated Post 3', body: 'Updated body of Post 3');
+  final updatePost = Post(
+      id: updateID, title: 'Updated Post 3', body: 'Updated body of Post 3');
   await APIService().updatePost(updateID, updatePost);
 
   //PUT request
   final deleteID = '3';
   await APIService().deletePost(postID: deleteID);
-  
 }
