f2cbd03f7dda3659c1710e9ecdc1f362e2347314
Mereke Sabitov <merekesabitov@gmail.com>
2024-02-15 12:15:20 +0500

    Stream

--- Learning_Dart/bin/08(3)_Stream/stream.dart
+++ Learning_Dart/bin/08(3)_Stream/stream.dart
@@ -1,25 +1,49 @@
 import 'dart:async';
 
-void main() {
+void main() async {
+  //await readStreamAsyncForLoop();
+  //readStream();
+}
 
+Future<void> readStreamAsyncForLoop() async {
+  final stream = Stream.fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  try {
+    await for (final data in stream) {
+      print('Получено: $data');
+      //throw Exception('ошибка');
+    }
+  } on Exception catch (error) {
+    print(error);
+  } finally {
+    print('Все события завершены');
+  }
+}
+
+void readStream() {
   StreamSubscription<int>? subscription;
-  
-  final stream = Stream.periodic(Duration(seconds: 1), (tick) => tick);
+
+  final stream = Stream.periodic(Duration(seconds: 1), (tick) {
+    if (tick == 4) throw Exception('Исключение в событии');
+    return tick;
+  }).take(5);
 
   print('Поток запущен');
-  subscription = stream.listen((data) {
-    print(data);
-  });
 
-  Future.delayed(Duration(seconds: 3), () {
-    subscription?.isPaused;
-    print('Поток приостановлен');
-  });
+  subscription = stream.listen((data) => print('Получено: $data'),
+      onDone: () => print('Все события завершены'),
+      onError: (Object error) => print(error),
+      cancelOnError: true);
 
-  Future.delayed(Duration(seconds: 5), () {
-    subscription?.resume();
-    print('Поток возобновлен');
-  });
+  // Future.delayed(Duration(seconds: 3), () {
+  //   subscription?.isPaused;
+  //   print('Поток приостановлен');
+  // });
+
+  // Future.delayed(Duration(seconds: 5), () {
+  //   subscription?.resume();
+  //   print('Поток возобновлен');
+  // });
 
   Future.delayed(Duration(seconds: 8), () {
     subscription?.cancel();
